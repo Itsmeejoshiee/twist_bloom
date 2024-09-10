4cf1b8d4cb06550ee1a93f9f324c0cbdaf9ae3e5
BrkerMaus <103913208+BrkerMaus@users.noreply.github.com>
2024-09-10 22:44:43 +0800

    Add files via upload

---
+++ lib/product.dart
@@ -0,0 +1,96 @@
+import 'package:flutter/material.dart';
+
+class ProductCard extends StatelessWidget {
+  final String imageUrl;
+  final String title;
+  final double price;
+
+  const ProductCard({
+    Key? key,
+    required this.imageUrl,
+    required this.title,
+    required this.price,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      elevation: 3,
+      shape: RoundedRectangleBorder(
+        borderRadius: BorderRadius.circular(15.0),
+      ),
+      color: const Color(0xFFE0D19E), // Background color
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Container(
+            width: double.infinity, // Ensures the image takes up full width
+            height: 170, // Fixed height to avoid overflow
+            padding: const EdgeInsets.all(8.0), // Padding around the image
+            child: ClipRRect(
+              borderRadius: BorderRadius.circular(15.0),
+              child: Image.asset(
+                imageUrl,
+                fit: BoxFit.cover,
+              ),
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text(
+                  title,
+                  style: const TextStyle(
+                    fontSize: 12,
+                  ),
+                  overflow: TextOverflow.ellipsis, // Handle long text
+                ),
+                const SizedBox(height: 4),
+                Text(
+                  '\$${price.toStringAsFixed(2)}', // Fixed the dollar sign
+                  style: const TextStyle(
+                    fontSize: 12,
+                    color: Colors.green,
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+
+class ProductSquareCard extends StatelessWidget {
+  final String imageUrl;
+
+  const ProductSquareCard({
+    Key? key,
+    required this.imageUrl,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: 124, // Full width of the parent container
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(16.0), // Circular sides
+        boxShadow: [
+          BoxShadow(
+            color: Colors.black.withOpacity(0.2), // Shadow color
+            blurRadius: 8.0, // Shadow blur radius
+            offset: Offset(0, 4), // Shadow offset
+          ),
+        ],
+        image: DecorationImage(
+          image: AssetImage(imageUrl),
+          fit: BoxFit.fill,
+        ),
+      ),
+    );
+  }
+}
