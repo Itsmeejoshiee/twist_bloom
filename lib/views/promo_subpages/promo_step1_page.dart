941a1cbdc5e0ec80817e83213d6769de73acaea4
Cronus <mrt.lusung@gmail.com>
2024-10-11 16:33:41 +0800

    initial commit

--- lib/views/promo_subpages/promo_step1_page.dart
+++ lib/views/promo_subpages/promo_step1_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'promo_step2_page.dart';
 import 'package:twist_bloom/widgets/product.dart';
 
 class PromoStep1Page extends StatefulWidget {
@@ -12,23 +13,33 @@ class _PromoStep1Page extends State<PromoStep1Page> {
   List<Map<String, dynamic>> products = [
     {
       'imageUrl': 'assets/icon/product/product1.jpg',
-      'title': 'Product 1',
-      'price': 69.99,
+      'title': 'Tulip',
+      'price': 65.0,
     },
     {
       'imageUrl': 'assets/icon/product/product2.jpg',
-      'title': 'Product 2',
-      'price': 29.99,
+      'title': 'Sunflower',
+      'price': 75.0,
     },
     {
       'imageUrl': 'assets/icon/product/product1.jpg',
-      'title': 'Product 1',
-      'price': 49.99,
+      'title': 'Poppy (2 Stems)',
+      'price': 45.0,
     },
     {
       'imageUrl': 'assets/icon/product/product2.jpg',
-      'title': 'Product 2',
-      'price': 59.99,
+      'title': 'Rose',
+      'price': 70.0,
+    },
+    {
+      'imageUrl': 'assets/icon/product/product2.jpg',
+      'title': 'Lily',
+      'price': 65.0,
+    },
+    {
+      'imageUrl': 'assets/icon/product/product2.jpg',
+      'title': 'Gerbera',
+      'price': 70.0,
     },
   ];
 
@@ -50,7 +61,13 @@ class _PromoStep1Page extends State<PromoStep1Page> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('STEP 1'),
+        title: const Text('MIX & MATCH'),
+        backgroundColor: Colors.transparent, // Make app bar transparent
+        elevation: 0, // Remove shadow
+        leading: IconButton(
+          icon: const Icon(Icons.arrow_back, color: Colors.black),
+          onPressed: () => Navigator.of(context).pop(),
+        ),
         actions: [
           IconButton(
             icon: Image.asset('assets/icon/filter.png'),
@@ -61,11 +78,22 @@ class _PromoStep1Page extends State<PromoStep1Page> {
         ],
       ),
       body: Padding(
-        padding: const EdgeInsets.fromLTRB(8, 1, 8, 8),
+        padding: const EdgeInsets.all(8.0),
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.start,
           children: [
-            // Search Bar - expanded by default
+            // Banner image at the top
+            Image.asset(
+              'assets/promo_background.png',
+              height: 120.0,
+              width: double.infinity,
+              fit: BoxFit.cover,
+            ),
+            const SizedBox(height: 16.0),
+            const Text(
+              'Step 1: Pick your flower (1 Flower)',
+              style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+            ),
             const SizedBox(height: 16.0),
             Expanded(
               child: GridView.builder(
@@ -73,7 +101,7 @@ class _PromoStep1Page extends State<PromoStep1Page> {
                   crossAxisCount: 2,
                   crossAxisSpacing: 10,
                   mainAxisSpacing: 10,
-                  childAspectRatio: 3.16 / 4,
+                  childAspectRatio: 3.16 / 4, // Aspect ratio similar to image
                 ),
                 itemCount: filteredProducts.length,
                 itemBuilder: (context, index) {
@@ -84,6 +112,72 @@ class _PromoStep1Page extends State<PromoStep1Page> {
                   );
                 },
               ),
+            ),
+          ],
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          Navigator.push(
+            context,
+            MaterialPageRoute(builder: (context) => PromoStep2Page()),
+          );
+        },
+        child: const Icon(Icons.arrow_forward),
+        backgroundColor: Color(0xFFFF92B2),
+      ),
+    );
+  }
+}
+
+// ProductCard widget to reflect design changes
+class ProductCard extends StatelessWidget {
+  final String imageUrl;
+  final String title;
+  final double price;
+
+  const ProductCard({
+    required this.imageUrl,
+    required this.title,
+    required this.price,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      shape: RoundedRectangleBorder(
+        borderRadius: BorderRadius.circular(10),
+      ),
+      elevation: 4,
+      child: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            Expanded(
+              child: Image.asset(
+                imageUrl,
+                fit: BoxFit.contain,
+              ),
+            ),
+            const SizedBox(height: 8.0),
+            Text(
+              title,
+              style: const TextStyle(
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            const SizedBox(height: 4.0),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Text('₱ $price'),
+                Row(
+                  children: const [
+                    Icon(Icons.favorite_border, size: 16),
+                  ],
+                ),
+              ],
             ),
           ],
         ),
---
+++ lib/views/promo_subpages/promo_step2_page.dart
@@ -0,0 +1,181 @@
+import 'package:flutter/material.dart';
+import 'package:twist_bloom/widgets/product.dart';
+
+class PromoStep2Page extends StatefulWidget {
+  const PromoStep2Page({super.key});
+
+  @override
+  _PromoStep2Page createState() => _PromoStep2Page();
+}
+
+class _PromoStep2Page extends State<PromoStep2Page> {
+  List<Map<String, dynamic>> products = [
+    {
+      'imageUrl': 'assets/icon/product/product1.jpg',
+      'title': 'Lavender',
+      'price': 45.0,
+    },
+    {
+      'imageUrl': 'assets/icon/product/product2.jpg',
+      'title': 'Eucalyptus',
+      'price': 30.0,
+    },
+    {
+      'imageUrl': 'assets/icon/product/product1.jpg',
+      'title': 'Leather Fern',
+      'price': 45.0,
+    },
+    {
+      'imageUrl': 'assets/icon/product/product2.jpg',
+      'title': "Baby's Breath (2 Stems)",
+      'price': 35.0,
+    },
+    {
+      'imageUrl': 'assets/icon/product/product2.jpg',
+      'title': 'Waxflower (2 Stems)',
+      'price': 35.0,
+    },
+  ];
+
+  List<Map<String, dynamic>> filteredProducts = [];
+
+  @override
+  void initState() {
+    super.initState();
+    filteredProducts = products;
+  }
+
+  void filterProducts() {
+    setState(() {
+      filteredProducts.sort((a, b) => a['price'].compareTo(b['price']));
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('MIX & MATCH'),
+        backgroundColor: Colors.transparent, // Make app bar transparent
+        elevation: 0, // Remove shadow
+        leading: IconButton(
+          icon: const Icon(Icons.arrow_back, color: Colors.black),
+          onPressed: () => Navigator.of(context).pop(),
+        ),
+        actions: [
+          IconButton(
+            icon: Image.asset('assets/icon/filter.png'),
+            onPressed: () {
+              filterProducts();
+            },
+          ),
+        ],
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            // Banner image at the top
+            Image.asset(
+              'assets/promo_background.png',
+              height: 120.0,
+              width: double.infinity,
+              fit: BoxFit.cover,
+            ),
+            const SizedBox(height: 16.0),
+            const Text(
+              'Step 2: Pick your filler (1 Filler)',
+              style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+            ),
+            const SizedBox(height: 16.0),
+            Expanded(
+              child: GridView.builder(
+                gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+                  crossAxisCount: 2,
+                  crossAxisSpacing: 10,
+                  mainAxisSpacing: 10,
+                  childAspectRatio: 3.16 / 4, // Aspect ratio similar to image
+                ),
+                itemCount: filteredProducts.length,
+                itemBuilder: (context, index) {
+                  return ProductCard(
+                    imageUrl: filteredProducts[index]['imageUrl'],
+                    title: filteredProducts[index]['title'],
+                    price: filteredProducts[index]['price'],
+                  );
+                },
+              ),
+            ),
+          ],
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          Navigator.push(
+            context,
+            MaterialPageRoute(builder: (context) => PromoStep2Page()),
+          );
+        },
+        child: const Icon(Icons.arrow_forward),
+        backgroundColor: Color(0xFFFF92B2),
+      ),
+    );
+  }
+}
+
+// ProductCard widget to reflect design changes
+class ProductCard extends StatelessWidget {
+  final String imageUrl;
+  final String title;
+  final double price;
+
+  const ProductCard({
+    required this.imageUrl,
+    required this.title,
+    required this.price,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      shape: RoundedRectangleBorder(
+        borderRadius: BorderRadius.circular(10),
+      ),
+      elevation: 4,
+      child: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            Expanded(
+              child: Image.asset(
+                imageUrl,
+                fit: BoxFit.contain,
+              ),
+            ),
+            const SizedBox(height: 8.0),
+            Text(
+              title,
+              style: const TextStyle(
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            const SizedBox(height: 4.0),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Text('₱ $price'),
+                Row(
+                  children: const [
+                    Icon(Icons.favorite_border, size: 16),
+                  ],
+                ),
+              ],
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
