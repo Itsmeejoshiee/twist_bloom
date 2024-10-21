b5d4ec1ed5b5b536674e3df258cd671181faea09
Christian <christianquiambs04@gmail.com>
2024-10-22 00:01:17 +0800

    Added Hive package for local preferences

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:twist_bloom/Views/landing_page.dart';
 import 'package:twist_bloom/Views/splash_screen.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'firebase_options.dart';
@@ -18,7 +19,7 @@ class MainApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return const MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: LoadingPage(),
+      home: HomePage(),
     );
   }
 }
