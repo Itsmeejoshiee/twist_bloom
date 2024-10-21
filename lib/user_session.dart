285941b51f37825b835bd7e944cae9d4ac16951f
BrkerMaus <emmanc590@gmail.com>
2024-10-21 18:26:55 +0800

    added user_session

---
+++ lib/user_session.dart
@@ -0,0 +1,19 @@
+class UserSession {
+  UserSession._internal();
+
+  static final UserSession _instance = UserSession._internal();
+
+  factory UserSession() {
+    return _instance;
+  }
+
+  String? userId;
+
+  void setUserId(String id) {
+    userId = id;
+  }
+
+  String? getUserId() {
+    return userId;
+  }
+}
